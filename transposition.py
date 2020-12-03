54d59eb49826a8c0e9216cc1fd9c4b40d4b90c29
J-JUN_shim <wowns1537@gmail.com>
2020-12-04 03:06:12 +0900

    Update transposition.py

--- transposition.py
+++ transposition.py
@@ -3,48 +3,63 @@ import random
 
 class Transposition:
     def __init__(self, length):
-        self.length = length
+        self.length = length  # split(key) length
+        # make key(this is invisible because of underbar)
         self._keys = self._make_key(length)
 
+    # Transposition encryption
     def encryption(self, plainText):
         cipherText = ''
 
+        # split plain text and transpositioning with key
         for part in self._split_len(plainText):
-            for key in sorted(self._keys):
+            for key in sorted(self._keys):  # get sorted keys(same with range)
+                # and then get index for it
                 index = self._keys.index(key)
                 try:
+                    # join to cipher text
                     cipherText += part[index]
                 except IndexError:
+                    # continue for indexing error
                     continue
 
         return cipherText
 
+    # Transposition decryption
     def decryption(self, cipherText):
         decipherText = ''
 
+        # split cipher text and transpositioning with key
         for part in self._split_len(cipherText):
-            for key in self._keys:
+            for key in self._keys:  # get real position keys
+                # and then get index for it in sorted key
                 index = sorted(self._keys).index(key)
                 try:
+                    # join to decipher text
                     decipherText += part[index]
                 except IndexError:
+                    # continue for indexing error
                     continue
 
         return decipherText
 
-    def _split_len(self, seq):
+    # make split text
+    def _split_len(self, text):
         length = self.length
 
-        return [seq[i:i + length] for i in range(0, len(seq), length)]
+        return [text[i:i + length] for i in range(0, len(text), length)]
 
+    # make shuffle key randomly
     @staticmethod
     def _make_key(length):
         keys = list(range(length))
         random.shuffle(keys)
 
+        # tuple is immutable
         return tuple(keys)
 
 
+# for test
 if __name__ == '__main__':
     plain = input('문자 입력: ')
 
